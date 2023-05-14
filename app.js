f02277177410a5f0a3db4d79ad03b9889d533d53
bakosrozsa <bakosrozsa01@gmail.com>
2023-05-14 19:51:26 +0200

    Headerben lévő pilóták halványítása görgetésnél

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const translate = document.querySelectorAll(".translate");
+const header = document.querySelector("header");
+const hamilton = document.querySelector(".hamilton");
+const massa = document.querySelector(".massa");
+
+let header_height = header.offsetHeight;
+
 window.addEventListener('scroll', () => {
     let scroll = window.pageYOffset;
     translate.forEach(element => {
         let speed = element.dataset.speed;
-        element.style.transform = `translateY(${scroll * speed}px)`;
+        element.style.transform = `translateY(${scroll * speed}px)`;      
     })
+    hamilton.style.opacity = - scroll / (header_height / 2) + 1;
+    massa.style.opacity = - scroll / (header_height / 2) + 1;
 })
